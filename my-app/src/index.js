a7be66752498aeb4c74fa6e74479b452d75207a5
Zack Daniels <zackdaniels2522@gmail.com>
2022-11-11 16:01:58 -0500

    updates to index file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,23 +3,53 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-class Square extends React.Component {
-  render() {
+function Square(props) {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={props.onClick}>
       </button>
     );
   }
-}
+
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sqaures : Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      sqaures: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+    <Square 
+      value={this.state.squares[i]} 
+      onClick={() => this.handleClick(i)}
+    />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+     
 
     return (
       <div>
@@ -64,3 +94,23 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i =0; i < lines.length; i++) {
+    const [a, b, c,] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null
+}
